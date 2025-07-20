514d615d008f4c4f2a993a905d2ca597c9436a45
AkhilNagipogu <167780359+AkhilNagipogu@users.noreply.github.com>
2025-07-21 00:12:18 +0530

    Add files via upload

---
+++ Projects/int_to_binary.py
@@ -0,0 +1,11 @@
+print("Int to Binary Convetor")
+print("Ctrl + C to Exit")
+while 1 == 1:
+    NUM = int(input("Enter the Number : "))
+    d = NUM
+    binary = " "
+    while d >= 1:
+        r = d % 2
+        d = d // 2
+        binary = str(r) + binary
+    print("The Binary value for " + str(NUM) + " is : " + binary)
